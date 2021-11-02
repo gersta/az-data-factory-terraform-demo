17514a66d6143894d0e0d9907c61c505b364b9cd
Gerrit Stapper <gerrit.stapper@accenture.com>
2021-11-02 11:20:26 +0100

    Persist terraform state inside Azure
    Configure an Azure storage account as the backend for terraform so that the state file is pushed into the storage
    
    This ensures that the state is encrypted (TLS, AES) and secrets are not presented in clear text

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
         version = "~>2.0"
     }
   }
+
+  backend "azurerm" {
+        resource_group_name  = "terraform-backend"
+        storage_account_name = "terraformbackendetldemo"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
