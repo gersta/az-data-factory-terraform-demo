514471d985f403943bded66ab030ff352c52efc2
Gerrit Stapper <gerrit.stapper@accenture.com>
2021-11-02 08:23:22 +0100

    Add storage account into resource group

--- main.tf
+++ main.tf
@@ -16,7 +16,20 @@ resource "azurerm_resource_group" "terraform-demo" {
     location = "westeurope"
 
     tags = {
-        creater = "Terraform"
+        creator = "Terraform"
+        project = "terraform-demo"
+    }
+}
+
+resource "azurerm_storage_account" "terraform-demo" {
+    name = "terraformetldemo"
+    resource_group_name = azurerm_resource_group.terraform-demo.name
+    location = azurerm_resource_group.terraform-demo.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+
+    tags = {
+        creator = "Terraform"
         project = "terraform-demo"
     }
 }
